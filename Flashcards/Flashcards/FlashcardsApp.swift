d9ab86f4ffb3ed6b0c05f2a0ee72a3f3c653573e
Gabriel Siu <gabrielsiu831@gmail.com>
2023-05-22 11:49:27 -0400

    Add base project

---
+++ Flashcards/Flashcards/FlashcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardsApp.swift
+//  Flashcards
+//
+//  Created by Gabriel Siu on 2023-05-22.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardsApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
